7519343e0938d7affcfdfb9682387e5f7966c870
Jeffery Wilson <jeff@jeffalwilson.com>
2016-11-11 15:03:12 -0500

    Update insteon dimmer

--- homeassistant/custom_components/light/insteondimmeroh.py
+++ homeassistant/custom_components/light/insteondimmeroh.py
@@ -5,20 +5,28 @@ Openhab light platform that implements lights.
 import logging
 import requests
 
+import voluptuous as vol
+
 from homeassistant.components.light import (
-    ATTR_BRIGHTNESS, SUPPORT_BRIGHTNESS, Light)
+    ATTR_BRIGHTNESS, SUPPORT_BRIGHTNESS, Light, PLATFORM_SCHEMA)
+from homeassistant.const import (CONF_NAME, CONF_URL)
+import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "Openhab Insteon Dimmer"
-DEFAULT_BRIGHTNESS = "255"
 
 SUPPORT_INSTEON_DIMMER = (SUPPORT_BRIGHTNESS)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Required(CONF_URL): cv.string
+})
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """Setup Openhab Insteon Dimmer platform."""
 
-    resource = config.get('resource')
+    resource = config.get(CONF_URL)
 
     if resource is None:
         _LOGGER.error("Missing required variable: resource")
@@ -36,21 +44,48 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
 
     add_devices_callback([OpenhabLight(
         hass,
-        config.get('name', DEFAULT_NAME),
-        config.get('resource'),
-        config.get('brightness', DEFAULT_BRIGHTNESS))])
+        config.get(CONF_NAME),
+        config.get(CONF_URL))])
 
 class OpenhabLight(Light):
     """Provide an Openhab light."""
 
     # pylint: disable=too-many-arguments
-    def __init__(self, hass, name, resource, brightness):
+    def __init__(self, hass, name, resource):
         """Initialize the light."""
-        self._state = None
+        self._power = False
         self._hass = hass
         self._name = name
         self._resource = resource
-        self._brightness = brightness
+        self._brightness = 255
+        self.update()
+
+    def update(self):
+        request = requests.get(self._resource+"/state", timeout=10)
+
+        try:
+            brightness = int(float(request.text)/100*255)
+        except ValueError:
+            _LOGGER.error("Failed to update %s state, non-numeric value", self._resource)
+            return
+
+        if brightness > 0:
+            self._power = True
+            self._brightness = brightness
+        else:
+            self._power = False
+
+    def set_brightness(self, brightness):
+        onValue = str((brightness/255)*100)
+        request = requests.post(self._resource,
+                                data=onValue,
+                                timeout=10)
+        if (request.status_code != 200) and (request.status_code != 201):
+            _LOGGER.info("HTTP Status Code: %s", request.status_code)
+            _LOGGER.error("Can't set brightness of %s. Is resource/endpoint offline?", self._resource)
+            return True
+        else:
+            return False
 
     @property
     def should_poll(self):
@@ -70,14 +105,7 @@ class OpenhabLight(Light):
     @property
     def is_on(self):
         """Return true if light is on."""
-        request = requests.get(self._resource+"/state", timeout=10)
-        if not request.text.isnumeric():
-            self._state = False
-        elif int(float(request.text)) > 0:
-            self._state = True
-        else:
-            self._state = False
-        return self._state
+        return self._power
 
     @property
     def supported_features(self):
@@ -89,31 +117,17 @@ class OpenhabLight(Light):
         try:
             # Clamp brightness from 0 to 255
             brightness = \
-                max(0, min(kwargs.get(ATTR_BRIGHTNESS, int(self._brightness)), 255))
+                max(0, min(kwargs.get(ATTR_BRIGHTNESS, self._brightness), 255))
         except ValueError as ex:
             _LOGGER.warn("Failed to clamp brightness: %s", ex.__str__)
             return None
 
-        onValue = str((brightness/255)*100)
-        request = requests.post(self._resource,
-                                data=onValue,
-                                timeout=10)
-        if (request.status_code == 200) or (request.status_code == 201):
-            self._brightness = brightness
-            self._state = True
-        else:
-            _LOGGER.info("HTTP Status Code: %s", request.status_code)
-            _LOGGER.error("Can't turn on %s. Is resource/endpoint offline?", self._resource)
+        if self.set_brightness(brightness):
+            self._power = True if brightness > 0 else False
+            self._brightness = brightness if brightness > 0 else self._brightness
 
-        self.update_ha_state()
 
     def turn_off(self, **kwargs):
         """Turn the light off."""
-        request = requests.post(self._resource, data="0", timeout=10)
-        if (request.status_code == 200) or (request.status_code == 201):
-            self._state = False
-        else:
-            _LOGGER.error("Can't turn off %s. Is resource/endpoint offline?",
-                          self._resource)
-
-        self.update_ha_state()
+        if self.set_brightness(0):
+            self._power = False
