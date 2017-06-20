7455e11cb29419af54ce567f80cd6e483a28d38c
Jeffery Wilson <jeff@jeffalwilson.com>
2017-06-20 00:28:43 -0400

    Use python_script for bathroom automation, improve automations

---
+++ homeassistant/python_scripts/bathroom_motion.py
@@ -0,0 +1,17 @@
+if hass.states.is_state('binary_sensor.everyone_at_home_sleeping', 'off'):
+    hass.services.call('light', 'turn_on', {'entity_id': 'light.bathroom'})
+
+    if hass.states.is_state('light.bedroom', 'on') and \
+            hass.states.is_state('light.closet', 'off'):
+        hass.services.call('light', 'turn_on', {'entity_id': 'light.closet'})
+
+else:
+    hass.services.call('switch', 'turn_off', {'entity_id':
+        'switch.bathroom_fluxer'}, true)
+    service_data = {
+        'entity_id': 'light.bathroom',
+        'brightness': 8,
+        'color_temp': 454,
+        'transition': 10
+    }
+    hass.services.call('light', 'turn_on', service_data)
