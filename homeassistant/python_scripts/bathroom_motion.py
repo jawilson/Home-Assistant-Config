4766eb4adbba83e04c8e51acb01fdf8a4b7a50ad
Jeffery Wilson <jeff@jeffalwilson.com>
2017-08-08 10:51:36 -0400

    Lots of automation, script, and light changes

--- homeassistant/python_scripts/bathroom_motion.py
+++ homeassistant/python_scripts/bathroom_motion.py
@@ -1,17 +1,19 @@
-if hass.states.is_state('binary_sensor.everyone_at_home_sleeping', 'off'):
+if hass.states.is_state('binary_sensor.everyone_at_home_sleeping', 'on') or (
+        hass.states.is_state('binary_sensor.sleeping_at_home', 'on') and
+        hass.states.is_state('group.all_lights', 'off')):
+    hass.services.call('switch', 'turn_off', {'entity_id':
+        'switch.bathroom_fluxer'}, True)
+    service_data = {
+        'entity_id': 'light.bathroom_mirror_2',
+        'brightness': 4,
+        'color_temp': 454,
+        'transition': 10
+    }
+    hass.services.call('light', 'turn_on', service_data)
+
+else:
     hass.services.call('light', 'turn_on', {'entity_id': 'light.bathroom'})
 
     if hass.states.is_state('light.bedroom', 'on') and \
             hass.states.is_state('light.closet', 'off'):
         hass.services.call('light', 'turn_on', {'entity_id': 'light.closet'})
-
-else:
-    hass.services.call('switch', 'turn_off', {'entity_id':
-        'switch.bathroom_fluxer'}, True)
-    service_data = {
-        'entity_id': 'light.bathroom_mirror_2',
-        'brightness': 8,
-        'color_temp': 454,
-        'transition': 10
-    }
-    hass.services.call('light', 'turn_on', service_data)
