2bf37450a4b7d755423b015cf7168d07176c45bc
Jeffery Wilson <jeff@jeffalwilson.com>
2017-10-14 01:40:31 -0400

    Move SHIELD Plex playing to python script

---
+++ homeassistant/python_scripts/shield_plex_playing.py
@@ -0,0 +1,34 @@
+hass.services.call('light', 'turn_off', {
+    'entity_id': [
+        'light.kitchen_lights',
+        'light.dining_lights',
+        'light.office_light'
+    ]
+})
+hass.services.call('light', 'turn_off', {
+    'transition': 5,
+    'entity_id': [
+        'light.liquor_cabinet_lights',
+        'light.osram_lightify_br_tunable_white_0008a2d5_3',
+        'light.bedroom',
+        'light.closet'
+    ]
+})
+
+bathroom_motion = int(hass.states.get('sensor.bathroom_alarm_level').state) > 0
+showering = float(hass.states.get('sensor.bathroom_relative_humidity').state) > 80
+
+if not bathroom_motion and not showering:
+    hass.services.call('light', 'turn_off', {
+        'transition': 5,
+        'entity_id': 'light.bathroom'
+    })
+
+hass.services.call('switch', 'turn_off', {
+    'entity_id': [
+        'switch.christmas_tree',
+        'switch.cat_water'
+    ]
+})
+
+
