fa264000eaed0a0529892c69ebffb5037d833a04
Jeffery Wilson <jeff@jeffalwilson.com>
2019-04-01 15:16:11 -0400

    Improvements

--- homeassistant/python_scripts/call_service_if_state.py
+++ homeassistant/python_scripts/call_service_if_state.py
@@ -1,6 +1,7 @@
 entity_ids = data.get('entity_id')
 state = data.get('state')
 service = data.get('service')
+call_data = data.get('data', {})
 
 if isinstance(entity_ids, str):
     entity_ids = [entity_ids]
@@ -13,4 +14,5 @@ for entity_id in entity_ids:
 domain, service = service.split('.')
 
 if action_entities:
-    hass.services.call(domain, service, {'entity_id': action_entities})
+    call_data['entity_id'] = action_entities
+    hass.services.call(domain, service, call_data)
